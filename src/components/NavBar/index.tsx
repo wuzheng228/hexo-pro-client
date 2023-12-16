ab6545e2d8e6064290d716210705282ed254c14e
wuzheng228 <769737584@qq.com>
2023-12-16 19:38:16 +0800

    add login out func

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -63,7 +63,7 @@ function Navbar({ show }: { show: boolean }) {
 
   function logout() {
     setUserStatus('logout');
-    window.location.href = '/login';
+    window.location.href = '/pro/login';
   }
 
   function onMenuItemClick(key) {
